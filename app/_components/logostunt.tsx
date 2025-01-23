e35da0badc3749fba82e96b51491ca820a3d7c67
yusuf-ishaku <kingshakzy@gmail.com>
2025-01-23 17:10:27 +0100

    Added new image and changed landing UI

--- app/_components/logostunt.tsx
+++ app/_components/logostunt.tsx
@@ -1,15 +1,13 @@
 "use client"
-import { Titillium_Web } from "next/font/google";
-
-const titillium = Titillium_Web({weight: "700", subsets: ["latin"]});
+// import { Titillium_Web } from "next/font/google";
+import Image from "next/image";
+// const titillium = Titillium_Web({weight: "700", subsets: ["latin"]});
 const LogoStunt = ({showing} : {showing: boolean}) => {
-
     console.log("showing", showing);
     return (
       <div className={` ${showing ? "" : "opacity-0 pointer-events-none"}  absolute duration-300 w-screen text-9xl bg-black h-screen flex flex-col items-center justify-center gap-4 top-0 left-0`}>
-        <h2 className={`${titillium.className} fade-out-0 duration-300 `}>
-          C- <span className="text-[#DAEBF7]">3P0</span>
-        </h2>
+       <Image className="w-full h-full bg-cover md:hidden" src="/video.gif" alt="100" width={100} height={100}/>
+       <Image className="w-full h-full bg-auto hidden md:block" src="/desktop.gif" alt="100" width={100} height={100}/>
       </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@
 import React from "react";
 import { Titillium_Web } from "next/font/google";
 import LogoStunt from "./_components/logostunt";
-import InfoSection from "./_components/infoboxes";
+// import InfoSection from "./_components/infoboxes";
 import LoginButton from "./_components/login-button";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 const titillium = Titillium_Web({
   weight: ["200", "300", "400", "600", "700", "900"],
@@ -20,7 +21,7 @@ const Graph = () => {
       setTimeout(() => {
         setIsShowingLogoStunt(false);
         videoRef.current?.play();
-      }, 2000);
+      }, 4000);
     }
   }, [isShowingLogoStunt, videoRef]);
 
@@ -29,34 +30,40 @@ const Graph = () => {
   }
   return (
     <div className={`w-full h-full matrixbg font-bold ${titillium.className}`}>
-      <div className="w-full md:hidden bg-white absolute h-full">
+      {/* <div className="w-full md:hidden bg-white absolute h-full">
         <video
           className="fixed w-full top-0 left-0 h-screen"
           ref={videoRef}
           src="/c3p02.mp4"
         />
-      </div>
+      </div> */}
 
-      <div className="flex flex-col items-center justify-center py-8 pb-4">
-        <div className="w-full h-full flex  flex-col items-center  gap-10">
-          <h3 className={cn("font-semibold text-xl", titillium.className)}>
-            C-P30
-          </h3>
-          <div className="flex px-3 justify-center gap-6 items-center md:w-1/2 flex-col text-center">
-            <h3 className="text-[#93C7B1]">Your AI-Powered Assistant</h3>
-            <h2
-              className={`md:text-4xl text-2xl leading-relaxed ${titillium.className}`}
-            >
-              Smarter Tools, Trending Insights, and Effortless Trades – All in
-              One Place
-            </h2>
-            <p className="text-xs font-light leading-relaxed">
-              Experience the future with C-3P0. Dive straight into a world of
-              trending ideas, seamless trades, and powerful AI integrations.
-            </p>
-            <LoginButton />
+      <div className="flex flex-col h-[100vh] items-center justify-center md:justify-start py-8 pb-4">
+        <h3 className={cn("font-semibold text-xl", titillium.className)}>
+          C-P30
+        </h3>
+        <div className="w-4/5 flex flex-col-reverse md:flex-row px-3 justify-center md:justify-around gap-6 items-center text-center mt-8">
+          <div className="w-full md:w-1/2 flex items-center justify-center mb-10">
+            <Image
+              src="/Group 66.svg"
+              alt="Image Profile"
+              width={100}
+              height={100}
+              className="w-4/5"
+            />
           </div>
-          <InfoSection />
+          <div className="w-full md:w-1/2 ">
+            <Image
+              src="/Group 65.png"
+              alt="Image Profile"
+              width={400}
+              height={200}
+              className="w-full"
+            />
+          </div>
+        </div>
+        <div className=" flex flex-col items-center mb-20">
+          <LoginButton />
         </div>
       </div>
     </div>
